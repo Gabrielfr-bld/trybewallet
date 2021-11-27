62274f24a39918edb661249805225151b1926e2c
gabriel <franklingabriel158@gmail.com>
2021-11-27 15:15:42 -0300

    Updates

---
+++ precache-manifest.d9643834abbc52510c6796602ff8644f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67fb114384cf99f5573d7c7afdb88974",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92489e82e71c96bcc342",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b15f596b304a507ac636",
+    "url": "/static/js/2.1b2980bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1b2980bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92489e82e71c96bcc342",
+    "url": "/static/js/main.0482d09c.chunk.js"
+  },
+  {
+    "revision": "fe617c2cb41aece9e721",
+    "url": "/static/js/runtime-main.e8df815d.js"
+  }
+]);
